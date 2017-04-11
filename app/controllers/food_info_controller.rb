c59f0b8c38110a1e3b4e220fa39ee970c9787eef
Samuel <sgonsal@ncsu.edu>
2017-04-11 11:08:04 -0400

    Added a form for inputting food info

--- app/controllers/food_info_controller.rb
+++ app/controllers/food_info_controller.rb
@@ -8,5 +8,15 @@ class FoodInfoController < ApplicationController
 		#food.food_item = 
 	end
 	def create
+		@food = FoodInfo.new
+		@food.food_item = params[:food_info][:food_item]
+		@food.calories = params[:food_info][:calories]
+		@food.carb = params[:food_info][:carb]
+		@food.protein = params[:food_info][:protein]
+		@food.fat = params[:food_info][:fat]
+
+		if @food.save
+			puts "Successfully saved"
+		end
 	end
 end
